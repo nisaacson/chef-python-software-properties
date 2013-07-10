afad90ec7b8fb493646c25e8b5750dcb3e678203
Noah Isaacson <noah@nisaacson.com>
2013-07-10 17:17:12 -0600

    added recipe to metadata

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,5 @@ description      'Installs/Configures python-software-properties package'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.3'
 
+recipe "python-software-properties::default", "Install the python-software-properties package"
+
