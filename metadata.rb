76f4cb11d3a1c638a28d5bc2eb048abbf7df6049
Noah Isaacson <noah@nisaacson.com>
2013-07-10 16:47:09 -0600

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email ''
 license          'All rights reserved'
 description      'Installs/Configures python-software-properties package'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.1.3'
 
